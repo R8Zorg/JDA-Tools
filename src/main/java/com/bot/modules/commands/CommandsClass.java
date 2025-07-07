e2f53606a1a30c2f5948ea3c8706644b2042266e
Hak1e <hakie.notiko@gmail.com>
2025-07-07 12:43:51 +0400

    Moved to another class

--- src/main/java/com/bot/modules/commands/CommandsClass.java
+++ src/main/java/com/bot/modules/commands/CommandsClass.java
@@ -3,11 +3,7 @@ package com.bot.modules.commands;
 import com.bot.core.annotations.Command;
 import com.bot.core.annotations.Option;
 import com.bot.core.annotations.SlashCommands;
-import com.bot.core.annotations.Subcommand;
-import com.bot.core.annotations.SubcommandGroup;
 
-import net.dv8tion.jda.api.entities.Member;
-import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 
 @SlashCommands
@@ -25,28 +21,4 @@ public class CommandsClass {
         event.reply(message).queue();
     }
 
-    @Command(description = "Get guild's owner")
-    public void guild(SlashCommandInteractionEvent event) {
-    }
-
-    @SubcommandGroup(parentName = "guild")
-    public void get(SlashCommandInteractionEvent event) {
-    }
-
-    @SubcommandGroup(parentName = "guild")
-    public void add(SlashCommandInteractionEvent event) {
-    }
-
-    @Subcommand(parentNames = "guild get")
-    public void owner(SlashCommandInteractionEvent event) {
-        Member owner = event.getGuild().getOwner();
-        event.reply("The owner of this guild is " + owner.getAsMention()).setEphemeral(true).queue();
-    }
-
-    @Subcommand(parentNames = "guild add", name = "owner")
-    public void add_owner(SlashCommandInteractionEvent event,
-            @Option(name = "member", description = "Member to add") Member member,
-            @Option(name = "role") Role role) {
-        event.reply(member.getAsMention() + " added to this guild owners").setEphemeral(true).queue();
-    }
 }
