25bb6821c5eee905c8f9dbca14312583693d6a9a
Hak1e <hakie.notiko@gmail.com>
2025-07-16 12:55:10 +0400

    Added JavaDoc comments

--- src/main/java/io/github/r8zorg/jdatools/annotations/Option.java
+++ src/main/java/io/github/r8zorg/jdatools/annotations/Option.java
@@ -6,11 +6,27 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Marks parameter as command's parameter.
+ * Marks parameter as command's parameter.<br>
+ * Primitive types are not supported, so use wrapper classes like Integer
+ * instead of int.
  * 
  * @param name        Parameter name
- * @param description Parameter description
+ * @param description Parameter description. "Description not provided." by
+ *                    default
  * @param required    Is parameter required. True by default
+ * 
+ *                    <pre>
+ *                    All possible options:
+ * Option(name = "string", required = false) String param1
+ * Option(name = "int", required = false) int param2
+ * Option(name = "long", required = false) Long param3
+ * Option(name = "bool", required = false) Boolean param4
+ * Option(name = "double", required = false) Double param5
+ * Option(name = "role", required = false) Role param6
+ * Option(name = "member", required = false) Member param7
+ * Option(name = "channel", required = false) Channel param8
+ * Option(name = "attachment", required = false) Attachment param9)
+ *                    </pre>
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.PARAMETER)
