40b1a09c0e2b048f387c66916f6210bd00801d8c
Hak1e <hakie.notiko@gmail.com>
2025-07-16 12:34:40 +0400

    Removed useless strings

--- src/main/java/io/github/r8zorg/jdatools/TypeOptions.java
+++ src/main/java/io/github/r8zorg/jdatools/TypeOptions.java
@@ -22,13 +22,9 @@ public class TypeOptions {
         {
             put(String.class, new OptionHandler(OptionType.STRING, OptionMapping::getAsString));
             put(Integer.class, new OptionHandler(OptionType.INTEGER, OptionMapping::getAsInt));
-            put(int.class, new OptionHandler(OptionType.INTEGER, OptionMapping::getAsInt));
             put(Long.class, new OptionHandler(OptionType.INTEGER, OptionMapping::getAsLong));
-            put(long.class, new OptionHandler(OptionType.INTEGER, OptionMapping::getAsLong));
             put(Boolean.class, new OptionHandler(OptionType.BOOLEAN, OptionMapping::getAsBoolean));
-            put(boolean.class, new OptionHandler(OptionType.BOOLEAN, OptionMapping::getAsBoolean));
             put(Double.class, new OptionHandler(OptionType.NUMBER, OptionMapping::getAsDouble));
-            put(double.class, new OptionHandler(OptionType.NUMBER, OptionMapping::getAsDouble));
             put(Role.class, new OptionHandler(OptionType.ROLE, OptionMapping::getAsRole));
             put(User.class, new OptionHandler(OptionType.USER, OptionMapping::getAsUser));
             put(Member.class, new OptionHandler(OptionType.USER, OptionMapping::getAsMember));
