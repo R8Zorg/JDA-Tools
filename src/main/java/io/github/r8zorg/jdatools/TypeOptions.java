d70742248a2bd6f195454da883f40e28bd6e6548
Hak1e <hakie.notiko@gmail.com>
2025-07-19 20:48:53 +0400

    Added more channel types support

--- src/main/java/io/github/r8zorg/jdatools/TypeOptions.java
+++ src/main/java/io/github/r8zorg/jdatools/TypeOptions.java
@@ -9,6 +9,16 @@ import net.dv8tion.jda.api.entities.Message.Attachment;
 import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.entities.channel.Channel;
+import net.dv8tion.jda.api.entities.channel.concrete.Category;
+import net.dv8tion.jda.api.entities.channel.concrete.ForumChannel;
+import net.dv8tion.jda.api.entities.channel.concrete.GroupChannel;
+import net.dv8tion.jda.api.entities.channel.concrete.MediaChannel;
+import net.dv8tion.jda.api.entities.channel.concrete.NewsChannel;
+import net.dv8tion.jda.api.entities.channel.concrete.PrivateChannel;
+import net.dv8tion.jda.api.entities.channel.concrete.StageChannel;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+import net.dv8tion.jda.api.entities.channel.concrete.ThreadChannel;
+import net.dv8tion.jda.api.entities.channel.concrete.VoiceChannel;
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 
@@ -27,8 +37,18 @@ public class TypeOptions {
             put(Role.class, new OptionHandler(OptionType.ROLE, OptionMapping::getAsRole));
             put(User.class, new OptionHandler(OptionType.USER, OptionMapping::getAsUser));
             put(Member.class, new OptionHandler(OptionType.USER, OptionMapping::getAsMember));
-            put(Channel.class, new OptionHandler(OptionType.CHANNEL, OptionMapping::getAsChannel));
             put(Attachment.class, new OptionHandler(OptionType.ATTACHMENT, OptionMapping::getAsAttachment));
+            put(Channel.class, new OptionHandler(OptionType.CHANNEL, OptionMapping::getAsChannel));
+            put(TextChannel.class, new OptionHandler(OptionType.CHANNEL, OptionMapping::getAsChannel));
+            put(VoiceChannel.class, new OptionHandler(OptionType.CHANNEL, OptionMapping::getAsChannel));
+            put(Category.class, new OptionHandler(OptionType.CHANNEL, OptionMapping::getAsChannel));
+            put(PrivateChannel.class, new OptionHandler(OptionType.CHANNEL, OptionMapping::getAsChannel));
+            put(NewsChannel.class, new OptionHandler(OptionType.CHANNEL, OptionMapping::getAsChannel));
+            put(ForumChannel.class, new OptionHandler(OptionType.CHANNEL, OptionMapping::getAsChannel));
+            put(GroupChannel.class, new OptionHandler(OptionType.CHANNEL, OptionMapping::getAsChannel));
+            put(MediaChannel.class, new OptionHandler(OptionType.CHANNEL, OptionMapping::getAsChannel));
+            put(StageChannel.class, new OptionHandler(OptionType.CHANNEL, OptionMapping::getAsChannel));
+            put(ThreadChannel.class, new OptionHandler(OptionType.CHANNEL, OptionMapping::getAsChannel));
         }
     };
 }
