d65e9c2e2684a0e82ca243233b47c7e2cd5c96b4
Hak1e <hakie.notiko@gmail.com>
2025-07-31 00:33:19 +0400

    RegisterListeners(JDA) -> getAllListeners. Registers SlashCommandHandler too

--- src/main/java/io/github/r8zorg/jdatools/ListenersManager.java
+++ src/main/java/io/github/r8zorg/jdatools/ListenersManager.java
@@ -1,6 +1,7 @@
 package io.github.r8zorg.jdatools;
 
-import io.github.r8zorg.jdatools.annotations.EventListeners;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,45 +9,50 @@ import org.slf4j.LoggerFactory;
 import io.github.classgraph.ClassGraph;
 import io.github.classgraph.ClassInfo;
 import io.github.classgraph.ScanResult;
-import net.dv8tion.jda.api.JDA;
+import io.github.r8zorg.jdatools.annotations.EventListeners;
 import net.dv8tion.jda.api.hooks.EventListener;
 
 /**
  *
- * Call {@link #registerListeners(JDA)} to register all listeners
+ * Call {@link #getAllListeners()} to register all listeners
  */
 public class ListenersManager {
     private final Logger logger = LoggerFactory.getLogger(ListenersManager.class);
     private final String packagesPath;
+    private final CommandsManager commandsManager;
 
     /**
      * 
      * @param packagesPath Path to directory with classes containing listeners
     */
-    public ListenersManager(String packagesPath) {
+    public ListenersManager(String packagesPath, CommandsManager commandsManager) {
         this.packagesPath = packagesPath;
+        this.commandsManager = commandsManager;
     }
 
     /**
      * Returns all listener classes annotated with
      * {@link io.github.r8zorg.jdatools.annotations.EventListeners}
      */
-    public void registerListeners(JDA jda) {
+    public Object[] getAllListeners() {
         try (ScanResult scanResult = new ClassGraph()
                 .enableClassInfo()
                 .enableAnnotationInfo()
                 .acceptPackages(packagesPath)
                 .scan()) {
+            List<Object> listeners = new ArrayList<>();
             for (ClassInfo classInfo : scanResult.getClassesWithAnnotation(EventListeners.class.getName())) {
                 Class<?> clazz = classInfo.loadClass();
                 if (EventListener.class.isAssignableFrom(clazz)) {
                     try {
-                        jda.addEventListener(clazz.getDeclaredConstructor().newInstance());
+                        listeners.add(clazz.getDeclaredConstructor().newInstance());
                     } catch (Exception e) {
                         logger.error(e.toString());
                     }
                 }
             }
+            listeners.add(new SlashCommandsHandler(commandsManager));
+            return listeners.toArray(new EventListener[0]);
         }
     }
 }
