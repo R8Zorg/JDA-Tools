6f2d360062425dabb872da48c8d48322a5e91066
Hak1e <hakie.notiko@gmail.com>
2025-07-31 00:48:59 +0400

    Added javadocs

--- src/main/java/io/github/r8zorg/jdatools/annotations/OwnerOnly.java
+++ src/main/java/io/github/r8zorg/jdatools/annotations/OwnerOnly.java
@@ -5,6 +5,23 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Allow command access only to bot's owner. Use
+ * OwnersRegistry.setOwners(Collection<Long> ids)<br>
+ *
+ * <p>
+ * <strong>Example:</strong>
+ * </p>
+ *
+ * <pre>
+ * <code>
+ *   &#64;Command(description = "Owner commands")
+ *   &#64;OwnerOnly
+ *   public void owners(SlashCommandInteractionEvent event) { ... }
+ * </code>
+ * </pre>
+ * 
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface OwnerOnly {
