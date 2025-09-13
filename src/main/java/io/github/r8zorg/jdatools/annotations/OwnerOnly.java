b0cbb882dd86d4339a7b12c5e5b1faf0982bd394
Hak1e <hakie.notiko@gmail.com>
2025-09-14 01:26:25 +0400

    Added the ability to change accessErrorEmbed title, description, footer and image url

--- src/main/java/io/github/r8zorg/jdatools/annotations/OwnerOnly.java
+++ src/main/java/io/github/r8zorg/jdatools/annotations/OwnerOnly.java
@@ -8,7 +8,16 @@ import java.lang.annotation.Target;
 /**
  * Allow command access only to bot's owner. Use
  * OwnersRegistry.setOwners()<br>
+ * Responds with an embed message if user is not in the owner list.<br>
  *
+ * <p>
+ * <strong>Parameters:</strong><br>
+ * <ul>
+ * <li><strong>title</strong> — Embed title.</li>
+ * <li><strong>description</strong> — Embed description. Default is: "You cannot use this command."</li>
+ * <li><strong>footer</strong> — Embed footer.</li>
+ * <li><strong>imageUrl</strong> — Embed image url.</li>
+ * </ul>
  * <p>
  * <strong>Example:</strong>
  * </p>
@@ -20,9 +29,24 @@ import java.lang.annotation.Target;
  *   public void owners(SlashCommandInteractionEvent event) { ... }
  * </code>
  * </pre>
+ *
+ * <strong>2 Example:</strong>
+ * </p>
+ *
+ * <pre>
+ * <code>
+ *   &#64;Command(description = "Owner commands")
+ *   &#64;OwnerOnly(title = "Access error", footer = "This command is for bot's owner only")
+ *   public void owners(SlashCommandInteractionEvent event) { ... }
+ * </code>
+ * </pre>
  * 
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface OwnerOnly {
+    String title() default "";
+    String description() default "You cannot use this command.";
+    String footer() default "";
+    String imageUrl() default "";
 }
